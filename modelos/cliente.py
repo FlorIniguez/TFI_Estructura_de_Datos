fcfca15104276fb42267b9b2eaea0750f6e6bcd9
FlorIniguez <iniguezflorencia788@gmail.com>
2025-11-28 02:09:41 -0300

    eliminando clase cliente

--- modelos/cliente.py
+++
@@ -1,8 +0,0 @@
-class Cliente:
-    def __init__(self, id_cliente,nombre,direccion):
-        self.id_cliente = id_cliente
-        self.nombre = nombre
-        self.direccion = direccion
-        
-    def __str__(self):
-        return f"Cliente {self.id_cliente} - {self.nombre}"
